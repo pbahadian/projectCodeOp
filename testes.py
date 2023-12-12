ca3b133ab4cf44c45e6d0aa99a2a5d7c296c4fbf
pbahadian <46781071+pbahadian@users.noreply.github.com>
2023-12-12 01:51:57 +0100

    Add files via upload

--- testes.py
+++ testes.py
@@ -15,7 +15,8 @@
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
+
+
 data = pd.read_csv("HDR21-22_Composite_indices_complete_time_series.csv", header = 0)
 df = pd.DataFrame(data)
 
